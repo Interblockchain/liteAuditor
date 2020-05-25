caf7cf90d12fa2d44152230468a80e6327a1b54f
Paul Boulanger <paul.boulanger5@gmail.com>
2020-05-25 10:42:20 -0400

    Changed conftable

--- config/confTable.js
+++ config/confTable.js
@@ -2,10 +2,10 @@
 // we must wait fro at least this number of confirmation
 // before initiating Tx in target blockchain
 global.confTable = {
-    "BTC": 0,   // 6  (60 min)
-    "BCH": 0,   // 6  (60 min)
-    "LTC": 0,   // 12 (30 min)
-    "ETH": 1,   // 30 (6 min)
+    "BTC": 6,   // 6  (60 min)
+    "BCH": 15,   // 6  (60 min)
+    "LTC": 12,   // 12 (30 min)
+    "ETH": 30,   // 30 (6 min)
     "XLM": 1,
     "XRP": 1,
     "EOS": 1,
